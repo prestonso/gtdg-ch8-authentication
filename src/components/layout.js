8cf76b153eb61548d474964cfc8fd2763ed32fb7
Preston So <pso@post.harvard.edu>
2021-10-04 11:42:43 -0300

    Add layout component

---
+++ src/components/layout.js
@@ -0,0 +1,11 @@
+// src/components/layout.js
+import React from "react"
+import Nav from "./nav"
+
+const Layout = ({ children }) => (
+  <>
+    <Nav />
+    {children}
+  </>
+)
+export default Layout
